cfe3b8b0b332e4b45ec7cb301df7c6c8518d33af
pakueamb <pakueamb@gmail.com>
2018-01-20 17:43:10 -0500

    used classes to shorten code

---
+++ Music_piano.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+"By Andre Akue"
+
+import RPi.GPIO as GPIO
+import time
+import pygame
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
+pygame.mixer.init()
+
+class keys:
+
+    def __init__(self, button, led, sound):
+        self.key = button
+        self.led = led
+        self.note = pygame.mixer.Sound(sound)
+        GPIO.setup(self.key, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        GPIO.setup(self.led, GPIO.OUT, initial = 0)
+    
+    def press(self,key):
+		self.note.play(maxtime = 300)
+		GPIO.output(self.led, 1)
+		time.sleep(0.3)
+		GPIO.output(self.led, 0)
+		time.sleep(0.3)
+
+K1 = keys(20,21, "piano_effects/do.wav")
+K2 = keys(19,16, "piano_effects/re.wav")
+K3 = keys(6,12, "piano_effects/mi.wav")
+K4 = keys(5,(21,13), "piano_effects/fa.wav")
+K5 = keys(23,(21,16,12,25,24,13), "piano_effects/sol.wav")
+K6 = keys(22,25, "piano_effects/la.wav")
+K7 = keys(27,24, "piano_effects/si.wav")
+K8 = keys(17,13, "piano_effects/do-octave.wav")
+
+time.sleep(1)
+print("\n!!!Piano Ready!!!\n\nExit with 'Enter(Return)'")
+
+try:
+    for i in [K1,K2,K3,K4,K5,K6,K7,K8]:
+	GPIO.add_event_detect(i.key, GPIO.FALLING, callback = i.press, bouncetime = 500)
+    raw_input()
+    GPIO.cleanup()
+    print ("Clean Exit. Have a nice day!")
+    exit()
+    
+except KeyboardInterrupt:
+    print ("\nCode stopped by 'keyboard interrupt'. Have a nice day.")
+    GPIO.cleanup()
